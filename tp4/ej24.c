d4d7fe8c427fee4983f24d44ca9332231f3ddd51
Pablo Sanchez <pds.gomez@gmail.com>
2017-05-16 00:07:26 -0300

    avanzo con ejercicio 24

--- tp4/ej24.c
+++ tp4/ej24.c
@@ -2,5 +2,31 @@
 
 int main()
 {
-	
+	int nota1=0,nota2=0,nota3=0,faltas=0;
+
+	printf("Ingrese nota 1: \n");
+	scanf("%i", &nota1);
+
+	printf("Ingrese nota 2: \n");
+	scanf("%i", &nota2);
+
+	printf("Ingrese nota 3: \n");
+	scanf("%i", &nota3);
+
+	printf("Ingrese cantidad de faltas: \n");
+	scanf("%i", &faltas);
+
+	if(nota1 >= 7 && nota2 >= 7 && nota3 >= 7 && faltas < 5)
+	{
+		printf("Promociono");
+		
+	}else if(nota1 >= 4 && nota2 >= 4 && nota3 >= 4 && faltas < 5)
+
+	{
+		printf("Curso");
+	}else
+
+	{
+		printf("Reprobo\n");
+	}
 }
